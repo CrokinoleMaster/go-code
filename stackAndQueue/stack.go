1af6b8f119669ab456f45361800fac52f2676b45
huaruiwu <huarui.wu@gmail.com>
2014-09-22 18:45:58 -0700

    stack

--- stackAndQueue/stack.go
+++ stackAndQueue/stack.go
@@ -1,15 +1,25 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
 
 type integer int
 
 func main() {
+	s := new(Stack)
+	s.Push(10)
+	s.Push(6)
+	s.Push(3)
+	fmt.Println(s.Min())
 }
 
 // integer stack implementation using linkedlist
 type Stack struct {
 	top *node
+	min int
 }
 
 type node struct {
@@ -17,7 +27,31 @@ type node struct {
 	data int
 }
 
+// returns the minimum integer in the stack
+func (s *Stack) Min() int {
+	return s.min
+}
+
+func (s *Stack) String() string {
+	var res string
+	for n := s.top; n != nil; n = n.next {
+		if n.next != nil {
+			res += strconv.Itoa(n.data) + ", "
+		} else {
+			res += strconv.Itoa(n.data)
+		}
+	}
+	return res
+}
+
 func (s *Stack) Push(i int) *Stack {
+	if s.top == nil {
+		s.min = i
+	} else {
+		if i < s.min {
+			s.min = i
+		}
+	}
 	n := &node{next: nil, data: i}
 	n.next = s.top
 	s.top = n
